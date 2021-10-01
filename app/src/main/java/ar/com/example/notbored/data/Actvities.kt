da48afcdf4ee0687f582890c56d918acfddd9b88
Pablo Beltran <pablonqn55@gmail.com>
2021-10-01 20:28:07 -0300

    eliminated function unused

--- app/src/main/java/ar/com/example/notbored/data/Actvities.kt
+++ app/src/main/java/ar/com/example/notbored/data/Actvities.kt
@@ -1,17 +1,14 @@
 package ar.com.example.notbored.data
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
 data class Category(
     val categoryName:String = "",
     val activities:List<RecreationalActivity>
 )
 
-@Parcelize
 data class RecreationalActivity(
     val activityName:String = "",
     val price : Double = 0.0,
     val participants:String = "2",
     val categoryName: String
-): Parcelable
+)
